9f1009cf9df01ee2ea757048704d2e68b2ab5048
Naveen-Gupta <nvngupta1992@gmail.com>
2017-08-30 06:53:16 +0530

    javascript global object

---
+++ Node-Global Object/node_globalObject.js
@@ -0,0 +1,12 @@
+console.log(__dirname);
+console.log(__filename);
+
+/*
+Output:
+
+Naveen@Naveen-PC MINGW32 ~/Desktop/Learning/Projects/Node
+$ node node_globalObject.js
+C:\Users\Naveen\Desktop\Learning\Projects\Node
+C:\Users\Naveen\Desktop\Learning\Projects\Node\node_globalObject.js
+
+*/
